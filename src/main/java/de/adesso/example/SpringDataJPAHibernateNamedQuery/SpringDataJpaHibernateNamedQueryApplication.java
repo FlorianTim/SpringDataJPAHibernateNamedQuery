caff7cdc0f9fceee020379b3b016dea223985be8
Unknown <florian@kroschewski.eu>
2018-07-17 09:30:42 +0200

    init

---
+++ src/main/java/de/adesso/example/SpringDataJPAHibernateNamedQuery/SpringDataJpaHibernateNamedQueryApplication.java
@@ -0,0 +1,12 @@
+package de.adesso.example.SpringDataJPAHibernateNamedQuery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaHibernateNamedQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaHibernateNamedQueryApplication.class, args);
+	}
+}
